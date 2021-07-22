a2a88c5b7703762ee2ef2128d9f70935c68908cb
mohan-vaishnavi <46642908+mohan-vaishnavi@users.noreply.github.com>
2021-07-22 11:49:26 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -7,19 +7,31 @@ namespace Statistics
     {
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
-            Stats _stats = new Stats();
-			var sum = 0f;
+            	Stats _stats = new Stats();
 			
-			numbers.Sort();
-			foreach (var num in numbers)
-				{
-					sum += num;		
-				}
-			_stats.average = sum/numbers.Count;
-			_stats.min = numbers[0];
-			_stats.max = numbers[numbers.Count-1];		
+			if (numbers.Count==0){
+				_stats.average = Double.NaN;
+				_stats.min = Double.NaN;
+				_stats.max = Double.NaN;	
 			
-			return _stats;	
+			}
+			
+			else{
+				var sum = 0f;
+			
+				numbers.Sort();
+				foreach (var num in numbers)
+					{
+						sum += num;		
+					}
+				_stats.average = sum/numbers.Count;
+				_stats.min = numbers[0];
+				_stats.max = numbers[numbers.Count-1];		
+			
+			}
+			
+			
+		return _stats;	
         }
     }
 }
